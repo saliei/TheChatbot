86b54e9d2472a70af55626efca9b7e5004014b27
Saeid Aliei <saeidaliei2019@gmail.com>
2021-10-02 15:28:14 +0200

    buffer read data

--- test/data_prep.py
+++ test/data_prep.py
@@ -2,6 +2,21 @@
 # read bz2, xz, zst compressed reddit data line by line, and process json files
 import pandas as pd
 import numpy as np
+import bz2
+import json
 import matplotlib.pyplot as plt
+
+BUFF_SIZE = 10
+
+filename = "RC_2005-12.bz2"
+
+with bz2.open(filename, "rt") as mdata:
+    lines = []
+    for i, line in enumerate(mdata):
+        if i == BUFF_SIZE: break
+        comments = json.loads(line)
+        lines.append(comments)
+
+
 
 
