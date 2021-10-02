053226506b13cab39ffaacb7efa7a2366427e3b5
Saeid Aliei <saeidaliei2019@gmail.com>
2021-10-02 11:08:16 +0200

    add the test data and barebone code

---
+++ test/data_prep.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+# read bz2, xz, zst compressed reddit data line by line, and process json files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
