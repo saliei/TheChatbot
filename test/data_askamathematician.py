c3364b7972554cc9ee316bb0888d5ad798c0a6fe
Saeid Aliei <saeidaliei2019@gmail.com>
2021-10-04 15:45:56 +0200

    extract date of the posts

--- test/data_askamathematician.py
+++ test/data_askamathematician.py
@@ -7,27 +7,6 @@ from bs4 import BeautifulSoup
 
 url = "https://www.askamathematician.com/"
 
-counter = 0
-
-# while True:
-    # r = requests.get(url)
-    # file_like_obj = io.StringIO(r.text) #Turns the requested output into a file like objet
-    # lines = file_like_obj.read()
-    # for l in lines:
-        # counter += 1 #Update the counter from proper filenames
-        # soup = BeautifulSoup(lines)
-        # findID = re.findall(r'post-(.*)\' itemprop', lines) #This retrieves each post's unique ID-number for the text.
-        # print(findID[0]) #for debugging
-        # div = soup.find(id="post-body-" + findID[0]) #This retrieves each post content
-        # print(div)
-        # with open(str(counter) + ".html", "w") as outputfile: #open file
-            # outputfile.write(str(div)) #write to file
-            # matchObj = re.findall(r'blog-pager-older-link\' href=\'(.*)\' id', lines) # This extract the "Older" post
-            # next_url = matchObj[0]
-            # print("Next URL for scraping: " + next_url)
-            # print("Press CTRL-C to exit the program.")
-            # url = next_url # This changes the variable in the beginning of the script
-
 # use a different user agent, the default agent is blocked
 page = requests.get(url, headers={"User-Agent": "XY"})
 print(page.status_code)
@@ -35,17 +14,12 @@ soup = BeautifulSoup(page.text, 'html.parser')
 qs_links_tags = soup.find_all('a', text = re.compile("Q:*"))
 qs_links = [atag["href"] for atag in qs_links_tags]
 
-# test for one of the questions
-# qpage = requests.get(qs_links[0], headers={"User-Agent": "XY"})
-# qsoup = BeautifulSoup(qpage.text, 'html.parser')
-# q = qsoup.find("h1", {"class": "entry-title"})
-# q = q.text
-
 def get_post_body(url):
     qpage = requests.get(url, headers={"User-Agent": "XY"})
     qsoup = BeautifulSoup(qpage.text, 'html.parser')
-    qtitle = qsoup.find("h1", {"class": "entry-title"})
-    qtitle = qtitle.text
+    qtitle = qsoup.find("h1", {"class": "entry-title"}).text
+
+    qdate = qsoup.find("span", {"class": "entry-date"}).text
 
     qcontent = ''
     content = qsoup.find("div", {"class": "entry-content"}).findAll('p')
