cb7c9b32a916b629e995ed7db58076c7aa6ced76
Saeid Aliei <saeidaliei2019@gmail.com>
2021-10-05 14:59:59 +0200

    write/read links to/from a file

--- test/data_vanqa.py
+++ test/data_vanqa.py
@@ -16,22 +16,33 @@ url_prefix = "https://van.physics.illinois.edu/qa/"
 subcat_links = [url_prefix + subcat["href"] for subcat in subcats]
 
 # find all Q&A links
-ques_links = []
-for subcat_link in subcat_links:
-    subcat_page = requests.get(subcat_link)
-    subcat_soup = BeautifulSoup(subcat_page.text, 'html.parser')
-    subcat_quns = subcat_soup.find_all("a", attrs = {"href": re.compile("listing.php*")})
-    subcat_quns_links = [url_prefix + subcat_quns_link["href"] for subcat_quns_link in subcat_quns]
-    # TODO: save links so we don't have to run finding links each time
-    for subcat_quns_link in subcat_quns_links:
-        print(subcat_quns_link)
-        ques_links.append(subcat_quns_link)
-        print("Total Q&A posts: {}".format(len(ques_links)))
+links_file = "posts_links_vanphysics.txt"
+try:
+    with open(links_file, "r") as link_file:
+        print("Reading links from the file '{}'.".format(links_file))
+        links = link_file.readlines()
+        ques_links = [link.rstrip() for link in links]
+        print("Total Q&A posts links: {}".format(len(ques_links)))
+except (FileNotFoundError ,IOError) :
+    print("Getting the links for all the Q&A posts.")
+    ques_links = []
+    with open(links_file, "w") as link_file:
+        for subcat_link in subcat_links:
+            subcat_page = requests.get(subcat_link)
+            subcat_soup = BeautifulSoup(subcat_page.text, 'html.parser')
+            subcat_quns = subcat_soup.find_all("a", attrs = {"href": re.compile("listing.php*")})
+            subcat_quns_links = [url_prefix + subcat_quns_link["href"] for subcat_quns_link in subcat_quns]
+            for subcat_quns_link in subcat_quns_links:
+                ques_links.append(subcat_quns_link)
+                link_file.write("{}\n".format(subcat_quns_link))
+                if len(ques_links) % 50 == 0:
+                    print("Total Q&A posts: {}".format(len(ques_links)), end='\r')
+        print("Total Q&A posts links: {}".format(len(ques_links)))
     
 # example Q&A page
 url_qa = "https://van.physics.illinois.edu/qa/listing.php?id=2353&t=magnetic-field-strength-at-the-end-of-a-solenoid"
 def get_qa(url_qa):
-    page_qa = reuqests.get(url_qa)
+    page_qa = requests.get(url_qa)
     soup_qa = BeautifulSoup(page_qa.text, 'html.parser')
     content_qa = {"url": url_qa}
     # find all Q&A sections
