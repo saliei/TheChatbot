cf282b54642a5d1b41349bf0ca35fb881cf3f291
Saeid Aliei <saeidaliei2019@gmail.com>
2021-10-05 11:51:29 +0200

    get the content of a Q&A post

--- test/data_vanqa.py
+++ test/data_vanqa.py
@@ -22,9 +22,36 @@ for subcat_link in subcat_links:
     subcat_soup = BeautifulSoup(subcat_page.text, 'html.parser')
     subcat_quns = subcat_soup.find_all("a", attrs = {"href": re.compile("listing.php*")})
     subcat_quns_links = [url_prefix + subcat_quns_link["href"] for subcat_quns_link in subcat_quns]
+    # TODO: save links so we don't have to run finding links each time
     for subcat_quns_link in subcat_quns_links:
         print(subcat_quns_link)
         ques_links.append(subcat_quns_link)
         print("Total Q&A posts: {}".format(len(ques_links)))
     
+# example Q&A page
+url_qa = "https://van.physics.illinois.edu/qa/listing.php?id=2353&t=magnetic-field-strength-at-the-end-of-a-solenoid"
+def get_qa(url_qa):
+    page_qa = reuqests.get(url_qa)
+    soup_qa = BeautifulSoup(page_qa.text, 'html.parser')
+    content_qa = {"url": url_qa}
+    # find all Q&A sections
+    qas = soup_qa.find_all("div", {"class": "listingletter"})
+    qas_text = []
+    for sec in qas:
+        if sec.string == "Q:":
+            ques = sec.next_sibling.text
+            qas_text.append(ques)
+        if sec.string == "A:":
+            answ = sec.next_sibling.text
+            qas_text.append(answ)
+
+    counter = 0
+    for i in range(len(qas_text)):
+        if i % 2 == 0:
+            counter += 1
+            content_qa["Q&A{}".format(counter)] = [qas_text[i], qas_text[i+1]]
+
+    return content_qa
+
+content = get_qa(url_qa)
 
