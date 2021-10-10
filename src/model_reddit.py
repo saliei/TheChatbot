444fd854d85fcb64e2c2c0ecc7078b8fc55eb708
Saeid Aliei <saeidaliei2019@gmail.com>
2021-10-10 21:30:33 +0200

    add acceptable and refine the comments further

--- src/model_reddit.py
+++ src/model_reddit.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 timeframe = '2005-12'
 spl_transaction = []
+buffering = 1000
 
 connection = sqlite3.connect('{}.db'.format(timeframe))
 print(connection.total_changes)
@@ -14,6 +15,7 @@ query = "CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY,\
         comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT,\
         unix INT, score INT)"
 c = connection.cursor()
+    
 
 def create_table():
     c.execute(query)
@@ -34,10 +36,38 @@ def find_parent(pid):
     except Exception as err:
             return False
 
+def find_existing_score(pid):
+    try:
+        query = "SELECT score from parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(query)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as err:
+        return False
+
 if __name__ == '__main__':
     create_table()
     row_counter = 0
     paired_rows = 0
     
     data_path = "../data/RC_{}".format(timeframe.split('-')[0], timeframe)
-    with open()
+    with open(data_path, buffering=buffering) as data:
+        for row in data:
+            row_counter += 1
+            row = json.loads(row)
+            parent_id = row["parent_id"]
+            body = format_data(row["body"])
+            created_utc = row["created_utc"]
+            score = row["score"]
+            comment_id = row["name"]
+            subreddit = row["subreddit"]
+            parent_data = find_parent(parent_id)
+
+            if score >= 2:
+                existing_comment_score = find_existing_score(parent_id)
+                # TODO: replace with comment that has the higher score
+                # if existing_comment_score and score > existing_comment_score:
+
